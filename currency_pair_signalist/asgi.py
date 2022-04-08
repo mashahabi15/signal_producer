c434fa21360d7e482fd9d2609d5cde7255ed8725
mashahabi15 <ma.shahabi15@gmail.com>
2022-04-08 16:26:56 +0430

    initial commit

---
+++ currency_pair_signalist/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for currency_pair_signalist project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'currency_pair_signalist.settings')
+
+application = get_asgi_application()
